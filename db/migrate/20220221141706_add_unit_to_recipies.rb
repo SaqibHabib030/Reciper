2af43e3a87fcc8196419210d03f7cfeb37f5953b
Saqib Habib <saqibhabib030@gmail.com>
2022-03-07 16:09:25 +0500

    Second commit

--- db/migrate/20220221141706_add_unit_to_recipies.rb
+++
@@ -1,5 +0,0 @@
-class AddUnitToRecipies < ActiveRecord::Migration[6.1]
-  def change
-    add_column :recipies, :unit, :string
-  end
-end
--- db/migrate/20220221141727_add_quantity_to_recipies.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToRecipies < ActiveRecord::Migration[6.1]
-  def change
-    add_column :recipies, :quantity, :float
-  end
-end
