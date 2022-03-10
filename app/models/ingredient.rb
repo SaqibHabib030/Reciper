a5fd500da03453bbfe3ddfbf17bb2b6d3fa7dfe5
Saqib Habib <saqibhabib030@gmail.com>
2022-03-10 21:42:01 +0500

    add front end validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,5 +4,5 @@ class Ingredient < ApplicationRecord
  has_many :recipies, through: :recipy_ingredients
  accepts_nested_attributes_for :recipy_ingredients, allow_destroy: true
 
- validates :name, presence: true
+ validates :name, presence: true, uniqueness: true
 end
